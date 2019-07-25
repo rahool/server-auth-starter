d927b41f2116345ab8cd657deb4882eff5ac38fe
Rahul Yadava <rahool@gmail.com>
2019-07-25 18:02:22 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const dummyRoute = require('./routes/dummy');
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.DB_CONNECT, 
+    { useNewUrlParser: true },
+    () => console.log('Connected to db'));
+
+//middleware
+app.use(express.json());
+app.use('/accounts', authRoute);
+app.use('/', dummyRoute);
+
+
+app.listen(3000, () => console.log('Server up on port 3000'));
